eff2533da1687361810608b3ca282f36776eb40b
Sharika's MacAir <sharikamahadevan2023@u.northwestern.edu>
2023-11-26 21:49:18 -0600

    Adding aws files

---
+++ streamlit-files-local/Welcome.py
@@ -0,0 +1,20 @@
+#########################################################################
+# Importing packages
+#########################################################################
+import streamlit as st
+import sys
+
+#########################################################################
+# main
+#########################################################################
+st.title('** Welcome to SocialPulse Insights **')
+st.header('Who are we?')
+st.write('Unlock the Power of Social Influence with SocialPulse Insights, the Premier Social Network Analytics Solution!')
+st.write('In the ever-evolving landscape of digital interactions, SocialPulse Insights stands as the forefront solution for companies seeking to harness the full potential of their social network presence. Our cutting-edge analytics platform provides a comprehensive and insightful view of your company\'s social media performance, allowing you to make data-driven decisions that propel your brand to new heights.')
+st.write('Why Choose SocialPulse Insights?')
+st.write('SocialPulse Insights is more than just a social network analytics tool; it\'s your strategic ally in navigating the dynamic world of social media. Elevate your brand, engage your audience, and drive business success with the unparalleled insights delivered by SocialPulse Insights.')
+st.write('Take control of your social narrative. Choose SocialPulse Insights today!')
+print()
+
+# eliminate traceback so we just get error message:
+sys.tracebacklimit = 0
---
+++ streamlit-files-local/pages/Perform_Network_Analytics.py
@@ -0,0 +1,62 @@
+########################################
+# Importing relevant packages
+########################################
+import os
+import logging
+import sys
+import base64
+from pathlib import Path
+from configparser import ConfigParser
+
+import streamlit as st
+
+# adding src to the system path
+sys.path.insert(0, os.getcwd())
+
+from src.study_network import execute_graph_operations
+
+########################################
+# main
+########################################
+
+st.set_page_config(
+    page_title="Go To Network Analytics",
+    page_icon="👍",
+)
+st.sidebar.success("Let's understand the Social Network.")
+
+# Setting config file 
+config_file = None
+try:
+    config_file = 'config/config.ini'
+    configur = ConfigParser()
+    configur.read(config_file)
+except Exception as err:
+    print("Error in reading config file")
+    print(err)
+
+print("Let's perform some graph operations.")
+
+st.title("Let's find about more about your social network.")
+st.write("Please ensure that your data is in the repository.")
+st.write("Please ensure that your data path is set correctly in the configuration.")
+
+network_choice = st.selectbox("Please select the network you would like to analyse?", ["EU Communication Network"])
+
+try:
+    user_prompt = st.text_input("What would you like to know about the network?")
+    if st.button("Get your answer."):
+        # Send prompt to function
+        response = execute_graph_operations(config_file, user_prompt, network_choice)
+
+        if response['statusCode'] == 200:
+            st.write(response["body"])
+        else:
+            st.write(response["body"])
+        
+    else:
+        print("You have gotten an error while trying to study network.")
+        st.write("You have gotten an error while trying to study network.")
+except Exception as err:
+    print("Error in graph operation")
+    print(err)
